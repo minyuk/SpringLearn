4d5ba1625aa35e57f18c24aed957dbc740c90e89
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-02-18 14:50:59 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+
+import hello.core.AppConfig;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
