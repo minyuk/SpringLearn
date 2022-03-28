435a274b431ba7b68f376724b0c3cb31a2850bc1
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-03-28 17:39:14 +0900

    Make Web 1day

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -55,13 +55,19 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
 
         itemRepository.save(item);
 
 //        model.addAttribute("item", item);
 
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
         return "basic/item";
     }
 
